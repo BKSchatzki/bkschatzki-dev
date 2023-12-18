bb276d6bcafabf47cba3d2c9cf105dc6ed46567e
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-12-18 12:33:04 -0800

    Remove react-responsive, and use display for menu icon, unnest carousel section

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,14 +4,12 @@ import NavLinks from "./NavLinks";
 import { Menu, X } from "lucide-react";
 
 import { useState } from "react";
-import { useMediaQuery } from "react-responsive";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const closeMenu = () => {
     setIsMenuOpen(false);
   };
-  const isTwSm = useMediaQuery({ query: "(min-width: 640px)" });
 
   return (
     <header className="fixed h-16 sm:h-24">
@@ -26,8 +24,10 @@ const Header = () => {
             onChange={() => setIsMenuOpen(!isMenuOpen)}
             checked={isMenuOpen}
           />
-          <Menu className="swap-off" size={isTwSm ? 48 : 32} />
-          <X className="swap-on" size={isTwSm ? 48 : 32} />
+          <Menu className="swap-off block sm:hidden" size={32} />
+          <X className="swap-on block sm:hidden" size={32} />
+          <Menu className="swap-off hidden sm:block" size={48} />
+          <X className="swap-on hidden sm:block" size={48} />
         </label>
         {isMenuOpen && <NavLinks closeMenu={closeMenu} />}
       </nav>
