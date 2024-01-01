7605d6c33c40c8e1caf69f5387eb269eb1fc7e65
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2024-01-01 01:06:40 -0800

    Update NavLinks and PageWrapper for 100dvh instead of 100vh

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -23,7 +23,7 @@ const NavLinks = ({
       {navType === "drawerMenu" && isMenuOpen && (
         <motion.ul
           key="backdrop"
-          className="fixed top-0 flex min-h-screen w-screen flex-col items-center justify-center gap-6 bg-black bg-opacity-80 bg-clip-padding text-center backdrop-blur-md backdrop-filter sm:gap-10"
+          className="fixed top-0 flex min-h-[100dvh] w-screen flex-col items-center justify-center gap-6 bg-black bg-opacity-80 bg-clip-padding text-center backdrop-blur-md backdrop-filter sm:gap-10"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0, transition: { delay: 0.25 } }}
