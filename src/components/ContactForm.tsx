5a9bed372c8a2f051f11f9f93e0b649427929231
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2024-01-01 01:41:47 -0800

    Remove motion from form component

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -4,15 +4,12 @@ import { motion } from "framer-motion";
 
 const ContactForm = () => {
   return (
-    <motion.form
+    <form
       name="contact"
       action="/success"
       method="POST"
       data-netlify="true"
       className="mt-6 flex w-full flex-col items-center justify-center"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { delay: 0.1 + 0.15 } }}
-      exit={{ opacity: 0 }}
     >
       <input type="hidden" name="form-name" value="contact" />
       <motion.label
@@ -88,7 +85,7 @@ const ContactForm = () => {
       >
         Send
       </motion.button>
-    </motion.form>
+    </form>
   );
 };
 
