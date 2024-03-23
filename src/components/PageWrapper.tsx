52a81330e1a51a6defbca7d1f8e961a7bd6ec7a2
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2024-03-23 14:53:23 -0700

    Change min-h of wrapper to h and remove scrollbar always on

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const PageWrapper = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="mx-auto flex min-h-[100dvh] w-11/12 flex-col items-center justify-center sm:py-24">
+    <div className="mx-auto flex h-[100dvh] w-11/12 flex-col items-center justify-center sm:py-24">
       {children}
     </div>
   );
