4bb79d5316373363d4aafce4eb360d90486ad525
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2025-03-17 01:07:43 -0700

    feat(styling): Cardify NameHeader and change primary color for light mode

--- src/components/sections/NameHeader.tsx
+++ src/components/sections/NameHeader.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
 
 import { H1 } from '@/components/typography';
+
+import { Card } from '../ui/card';
 
 interface Props {
   className?: string;
@@ -9,14 +12,20 @@ interface Props {
 
 const NameHeader = React.forwardRef<HTMLHeadingElement, Props>(({ className, ...props }, ref) => {
   return (
-    <H1
-      ref={ref}
-      className="header-space:px-6"
-    >
-      Brendan K. Schatzki
-      <br />
-      Software Engineer
-    </H1>
+    <Card className="py-6 text-balance">
+      <H1
+        ref={ref}
+        className="scroll-m-20 px-6 text-3xl font-bold tracking-tighter header-space:px-6 underline decoration-primary underline-offset-4 decoration-[0.3333rem]"
+      >
+        Brendan K. Schatzki
+        <br />
+        <span className="inline-block pt-3 text-2xl tracking-normal">
+          Full Stack Software Development Engineer
+          <br />
+          <span className="inline-block pt-3 text-xl font-normal tracking-normal">// React, Node, TypeScript, JavaScript, Next.js, PostgreSQL</span>
+        </span>
+      </H1>
+    </Card>
   );
 });
 NameHeader.displayName = 'NameHeader';
